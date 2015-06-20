33509a5f9a07a65f258cc92213780fda3bb541d1
camp007x <ts265047@fh-muenster.de>
2015-06-20 12:23:11 +0200

    change utils

--- ShelpSystem/ejbModule/de/shelp/util/RequestDtoAssembler.java
+++ ShelpSystem/ejbModule/de/shelp/util/RequestDtoAssembler.java
@@ -11,48 +11,62 @@ import de.shelp.dto.request.WishlistItemTO;
 import de.shelp.entities.Request;
 import de.shelp.entities.WishlistItem;
 
+/**
+ * Klasse zur Umwandlung von RequestObjekten in transportfähige Objekte.
+ * @author Jos Werner
+ *
+ */
 @Stateless
 public class RequestDtoAssembler {
 
-    /**
-     * EJB zur Erzeugung von DataTransferObjects
-     */
-    @EJB
-    private UserDtoAssembler userDtoAssembler;
+	/**
+	 * EJB zur Erzeugung von DataTransferObjects
+	 */
+	@EJB
+	private UserDtoAssembler userDtoAssembler;
 
-    /**
-     * EJB zur Erzeugung von DataTransferObjects
-     */
-    @EJB
-    private TourDtoAssembler tourDtoAssembler;
+	/**
+	 * EJB zur Erzeugung von DataTransferObjects
+	 */
+	@EJB
+	private TourDtoAssembler tourDtoAssembler;
 
-    public RequestTO makeDTO(Request request) {
-	RequestTO dto = new RequestTO();
+	/**
+	 * Methode zum Umwandeln des Objektes
+	 * @param request
+	 * @return Umgewandelte RequestTransferObjekt
+	 */
+	public RequestTO makeDTO(Request request) {
+		RequestTO dto = new RequestTO();
 
-	dto.setId(request.getId());
-	dto.setNotice(request.getNotice());
-	dto.setSourceUser(userDtoAssembler.makeDTO(request.getSourceUser()));
-	dto.setTargetUser(userDtoAssembler.makeDTO(request.getTargetUser()));
-	dto.setTour(tourDtoAssembler.makeDTO(request.getTour()));
-	dto.setUpdated(request.isUpdated());
-	dto.setWishes(makeDTO(request.getWishes()));
-	dto.setStatus(request.getStatus());
+		dto.setId(request.getId());
+		dto.setNotice(request.getNotice());
+		dto.setSourceUser(userDtoAssembler.makeDTO(request.getSourceUser()));
+		dto.setTargetUser(userDtoAssembler.makeDTO(request.getTargetUser()));
+		dto.setTour(tourDtoAssembler.makeDTO(request.getTour()));
+		dto.setUpdated(request.isUpdated());
+		dto.setWishes(makeDTO(request.getWishes()));
+		dto.setStatus(request.getStatus());
 
-	return dto;
-    }
-
-    public List<WishlistItemTO> makeDTO(List<WishlistItem> wishes) {
-
-	List<WishlistItemTO> dto = new ArrayList<>();
-
-	for (WishlistItem wishlistItem : wishes) {
-	    WishlistItemTO wishlistItemTO = new WishlistItemTO();
-	    wishlistItemTO.setId(wishlistItem.getId());
-	    wishlistItemTO.setChecked(wishlistItem.isChecked());
-	    wishlistItemTO.setText(wishlistItem.getText());
-	    dto.add(wishlistItemTO);
+		return dto;
 	}
-	return dto;
-    }
+/**
+ * Methode zum Umwandeln der Liste
+ * @param wishes
+ * @return Umgewandelte Liste
+ */
+	public List<WishlistItemTO> makeDTO(List<WishlistItem> wishes) {
+
+		List<WishlistItemTO> dto = new ArrayList<>();
+
+		for (WishlistItem wishlistItem : wishes) {
+			WishlistItemTO wishlistItemTO = new WishlistItemTO();
+			wishlistItemTO.setId(wishlistItem.getId());
+			wishlistItemTO.setChecked(wishlistItem.isChecked());
+			wishlistItemTO.setText(wishlistItem.getText());
+			dto.add(wishlistItemTO);
+		}
+		return dto;
+	}
 
 }
--- ShelpSystem/ejbModule/de/shelp/util/TourDtoAssembler.java
+++ ShelpSystem/ejbModule/de/shelp/util/TourDtoAssembler.java
@@ -13,74 +13,96 @@ import de.shelp.entities.Location;
 import de.shelp.entities.Request;
 import de.shelp.entities.Tour;
 
+/**
+ * Klasse zur Umwandlung von Touren
+ * 
+ * @author Jos Werner
+ *
+ */
 @Stateless
 public class TourDtoAssembler {
 
-    /**
-     * EJB zur Erzeugung von DataTransferObjects
-     */
-    @EJB
-    private UserDtoAssembler dtoAssembler;
+	/**
+	 * EJB zur Erzeugung von DataTransferObjects
+	 */
+	@EJB
+	private UserDtoAssembler dtoAssembler;
 
-    @EJB
-    private StateDtoAssembler stateDtoAssembler;
+	@EJB
+	private StateDtoAssembler stateDtoAssembler;
 
-    /**
-     * EJB zur Erzeugung von DataTransferObjects
-     */
-    @EJB
-    private UserDtoAssembler userDtoAssembler;
-    
-    /**
-     * EJB zur Erzeugung von DataTransferObjects
-     */
-    @EJB
-    private RequestDtoAssembler requestDtoAssembler;
+	/**
+	 * EJB zur Erzeugung von DataTransferObjects
+	 */
+	@EJB
+	private UserDtoAssembler userDtoAssembler;
 
-    public TourTO makeDTO(Tour tour) {
-	TourTO dto = new TourTO();
-	dto.setApprovalStatus(stateDtoAssembler.makeDTO(tour
-		.getApprovalStatus()));
-	dto.setLocation(makeDTO(tour.getLocation()));
-	dto.setCapacity(stateDtoAssembler.makeDTO(tour.getCapacity()));
-	dto.setPaymentCondition(stateDtoAssembler.makeDTO(tour
-		.getPaymentCondition()));
-	dto.setDeliveryCondition(stateDtoAssembler.makeDTO(tour
-		.getDeliveryCondition()));
-	dto.setTime(tour.getTime().getTime());
-	dto.setId(tour.getId());
-	dto.setOwner(dtoAssembler.makeDTO(tour.getOwner()));
-	dto.setStatus(tour.getStatus());
-	dto.setUpdated(tour.isUpdated());
-	dto.setRequest(makeDTO(tour.getRequest()));
+	/**
+	 * EJB zur Erzeugung von DataTransferObjects
+	 */
+	@EJB
+	private RequestDtoAssembler requestDtoAssembler;
 
-	return dto;
-    }
+	
+	/**
+	 *  Methode zum Umwandeln des Objektes
+	 * @param tour
+	 * @return Umgewandelts Objekt
+	 */
+	public TourTO makeDTO(Tour tour) {
+		TourTO dto = new TourTO();
+		dto.setApprovalStatus(stateDtoAssembler.makeDTO(tour
+				.getApprovalStatus()));
+		dto.setLocation(makeDTO(tour.getLocation()));
+		dto.setCapacity(stateDtoAssembler.makeDTO(tour.getCapacity()));
+		dto.setPaymentCondition(stateDtoAssembler.makeDTO(tour
+				.getPaymentCondition()));
+		dto.setDeliveryCondition(stateDtoAssembler.makeDTO(tour
+				.getDeliveryCondition()));
+		dto.setTime(tour.getTime().getTime());
+		dto.setId(tour.getId());
+		dto.setOwner(dtoAssembler.makeDTO(tour.getOwner()));
+		dto.setStatus(tour.getStatus());
+		dto.setUpdated(tour.isUpdated());
+		dto.setRequest(makeDTO(tour.getRequest()));
 
-    private List<RequestTO> makeDTO(List<Request> requests) {
-	List<RequestTO> dtos = new ArrayList<RequestTO>();
-	for (Request request : requests) {
-	    RequestTO dto = new RequestTO();
-	    
-	    dto.setId(request.getId());
-	    dto.setNotice(request.getNotice());
-	    dto.setSourceUser(userDtoAssembler.makeDTO(request.getSourceUser()));
-	    dto.setTargetUser(userDtoAssembler.makeDTO(request.getTargetUser()));
-	    dto.setUpdated(request.isUpdated());
-	    dto.setStatus(request.getStatus());
-	    dto.setWishes(requestDtoAssembler.makeDTO(request.getWishes()));
-	    dtos.add(dto);
+		return dto;
 	}
 
-	return dtos;
-    }
+	/**
+	 *  Methode zum Umwandeln des Objektes
+	 * @param requests 
+	 * @return  Umgewandelts Objekt
+	 */
+	private List<RequestTO> makeDTO(List<Request> requests) {
+		List<RequestTO> dtos = new ArrayList<RequestTO>();
+		for (Request request : requests) {
+			RequestTO dto = new RequestTO();
 
-    private LocationTO makeDTO(Location location) {
-	LocationTO dto = new LocationTO();
-	dto.setId(location.getId());
-	dto.setDescription(location.getDescription());
-	dto.setZipcode(location.getZipcode());
-	return dto;
-    }
+			dto.setId(request.getId());
+			dto.setNotice(request.getNotice());
+			dto.setSourceUser(userDtoAssembler.makeDTO(request.getSourceUser()));
+			dto.setTargetUser(userDtoAssembler.makeDTO(request.getTargetUser()));
+			dto.setUpdated(request.isUpdated());
+			dto.setStatus(request.getStatus());
+			dto.setWishes(requestDtoAssembler.makeDTO(request.getWishes()));
+			dtos.add(dto);
+		}
+
+		return dtos;
+	}
+
+	/**
+	 *  Methode zum Umwandeln des Objektes
+	 * @param location
+	 * @return Umgewandelts Objekt
+	 */
+	private LocationTO makeDTO(Location location) {
+		LocationTO dto = new LocationTO();
+		dto.setId(location.getId());
+		dto.setDescription(location.getDescription());
+		dto.setZipcode(location.getZipcode());
+		return dto;
+	}
 
 }
